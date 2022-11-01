f8714e3909cf125bd5c2e6685c0ec03e01d017ee
Bryce Fernandes <bryce.fernandes@neosoftmail.com>
2022-11-01 16:38:00 +0530

    zooombox

--- src/Components/ZoomBox.js
+++ src/Components/ZoomBox.js
@@ -1,7 +1,20 @@
 import React from "react";
+import "./styles/ZoomBox.css";
 
 function ZoomBox() {
-  return <div>ZoomBox</div>;
+  return (
+    <>
+      <div>
+        <h5>
+          Zoom <span style={{ color: "red" }}>100%</span>
+        </h5>
+        <hr />
+        <button>+</button>
+
+        <button>-</button>
+      </div>
+    </>
+  );
 }
 
 export default ZoomBox;
