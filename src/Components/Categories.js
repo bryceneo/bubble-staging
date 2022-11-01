a73cb92b9a7767620bafd863583c9ff87f27b5c1
Monali Gangane <monali.gangane1999@gmail.com>
2022-11-01 12:57:36 +0530

    category created

--- src/Components/Categories.js
+++ src/Components/Categories.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import CategoriesData from "./Data/CategoriesData";
 
 function Categories() {
-  return <div>Categories</div>;
+  const [catgories, setcategories] = useState(CategoriesData);
+  return (
+    <>
+      <div>
+        <h4>category</h4>
+        <div>
+          {catgories.map((category) => {
+            return <div>{category.name}</div>;
+          })}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Categories;
