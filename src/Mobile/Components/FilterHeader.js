bd06322ba572f20aa9ad6ed0ee04568e76fe6923
Bryce Fernandes <bryce.fernandes@neosoftmail.com>
2022-11-09 14:28:18 +0530

    css added for mobile view

--- src/Mobile/Components/FilterHeader.js
+++ src/Mobile/Components/FilterHeader.js
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
 import Modal from "react-modal";
 import FilterModal from "./FilterModal";
-
+import "./Styles/FilterHeader.css";
 function FilterHeader() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const openModal = () => setIsModalOpen(true);
   const closeModal = () => setIsModalOpen(false);
   return (
     <>
-      <div onClick={openModal}>FilterHeader</div>;
+      <div id="filter" onClick={openModal}>FilterHeader</div>
       <Modal isOpen={isModalOpen} onRequestClose={closeModal}>
         <div onClick={closeModal}>Close Filter</div>
         <FilterModal />
